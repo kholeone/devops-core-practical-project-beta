bb1d6f2e9ab475ccfacad8d586d2d27b75b0fc9d
Kholeo <kholeotaylor@hotmail.co.uk>
2020-12-03 22:08:59 +0000

    version 1.0

--- service-one/application/routes.py
+++ service-one/application/routes.py
@@ -4,12 +4,12 @@ import requests
 
 @app.route('/', methods=['GET'])   
 def index():
-    shoe_brand = requests.get("http://localhost:5001/shoe/brand")
-    shoe_model = requests.get("http://localhost:5002/shoe/model")
+    shoe_brand = requests.get("http://service-two:5001/shoe/brand")
+    shoe_model = requests.get("http://service-three:5002/shoe/model")
     shoe = shoe_brand.text+" "+str(shoe_model.text)
     return render_template('index.html', title='index', shoe=shoe)	
 
 @app.route('/information/<trainer>', methods=['GET', 'POST'])
 def information(trainer):
-    information = requests.post('http://localhost:5003/information', data=trainer)
+    information = requests.post('http://service-four:5003/information', data=trainer)
     return render_template('information.html', title='information', trainer=trainer, information=information.text)
