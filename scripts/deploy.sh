24189ba7a4d545808237f5cd2b74ad0a9a63f302
Kholeo <kholeotaylor@hotmail.co.uk>
2020-12-04 15:08:05 +0000

    created and setup roles for the docker swarm, with main.yaml's

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,3 +5,4 @@ docker pull kholeone/service-two:latest
 docker pull kholeone/service-three:latest
 docker pull kholeone/service-four:latest
 docker stack deploy --compose-file docker-compose.yaml snkrs-app
+docker service ls
