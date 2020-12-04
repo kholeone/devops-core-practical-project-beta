6245ac1de519e37edbdac66123631cb9ebf84520
Kholeo <kholeotaylor@hotmail.co.uk>
2020-12-04 15:46:27 +0000

    initial commit

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-cd ./service-one
-python3 -m pytest --cov application
-cd ..
+sudo apt update
+sudo apt install python3 python3-pip python3-venv -y
+python3 -m venv venf 
+. ./venf/bin/activate
+pip3 install -r requirements.txt
 
 cd ./service-two
 python3 -m pytest --cov application
