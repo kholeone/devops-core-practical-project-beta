f36e6d67450c77d0d3f366533912691ff50cf5fe
Kholeo Taylor <72978796+kholeone@users.noreply.github.com>
2020-12-06 13:43:14 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ssh manager << EOF
-docker pull kholeone/service-one:latest
-docker pull kholeone/service-two:latest
-docker pull kholeone/service-three:latest
-docker pull kholeone/service-four:latest
-docker pull nginx:alpine
-git clone https://github.com/kholeone/devops-core-practical-project-beta
-docker stack deploy --compose-file docker-compose.yaml snkrs-app
+sudo docker pull kholeone/service-one:latest
+sudo docker pull kholeone/service-two:latest
+sudo docker pull kholeone/service-three:latest
+sudo docker pull kholeone/service-four:latest
+sudo docker pull nginx:alpine
+sudo git clone https://github.com/kholeone/devops-core-practical-project-beta
+sudo docker stack deploy --compose-file docker-compose.yaml snkrs-app
 EOF
